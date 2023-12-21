cb00537ce3dd1efb1e0c6d745649bde5493b8434
Shiva Kumar Mangina <shiva2nani.mangina@gmail.com>
2023-12-21 22:36:02 +0530

    fix: Remove Border

--- src/components/PostComponent.tsx
+++ src/components/PostComponent.tsx
@@ -1,5 +1,5 @@
-import Avatar from 'react-avatar';
-import { ChatBubbleLeftEllipsisIcon, HeartIcon, ArrowsUpDownIcon } from "@heroicons/react/20/solid";
+import Avatar from "react-avatar";
+import { HeartIcon } from "@heroicons/react/20/solid";
 import PostPublishComponent from "./PostPublishComponent";
 
 const ellipseAddress = (address = "", width = 10) => {
@@ -16,16 +16,13 @@ const Post = ({ randomUUID, createdAt, title, content, images }: any) => {
         <a href="#" className="flex-shrink-0 group block">
           <div className="flex items-center">
             <div>
-            <Avatar name={ellipseAddress(randomUUID, 12)} size="50" round="100px" />
+              <Avatar name={ellipseAddress(randomUUID, 12).split("").reverse().join()} size="50" round="100px" />
             </div>
-            
 
             <div className="ml-3">
               <p className="text-base font-mono leading-6 font-medium text-black">
                 {ellipseAddress(randomUUID, 12)}
-                <span className="pl-3 text-sm leading-5 font-medium italic font-mono text-slate-600">
-                  {createdAt}
-                </span>
+                <span className="pl-3 text-sm leading-5 font-medium italic font-mono text-slate-600">{createdAt}</span>
               </p>
             </div>
           </div>
@@ -34,12 +31,6 @@ const Post = ({ randomUUID, createdAt, title, content, images }: any) => {
       <div className="pl-16">
         <p className="text-base width-auto font-medium text-[#0F1419] flex-shrink">{title}</p>
         <p className="text-base width-auto font-medium text-[#0F1419] flex-shrink">{content}</p>
-
-        {images && (
-          <div className="md:flex-shrink pr-6 pt-3">
-            <img className="rounded-lg w-full h-64" src={images} alt="Post Media" />
-          </div>
-        )}
 
         <div className="flex">
           <div className="w-full">
@@ -51,7 +42,7 @@ const Post = ({ randomUUID, createdAt, title, content, images }: any) => {
           </div>
         </div>
       </div>
-      <hr className="border-gray-600" />
+      <hr className="border-neutral-200" />
     </>
   );
 };
@@ -68,7 +59,7 @@ const PostAction = ({ icon: Icon }) => {
   );
 };
 
-const postActions = [{ icon: HeartIcon }, { icon: ArrowsUpDownIcon }, { icon: ChatBubbleLeftEllipsisIcon }];
+const postActions = [{ icon: HeartIcon }];
 
 const PostComponent = ({ publishPost, posts }: any) => {
   console.log(posts, "posts satya");
@@ -76,7 +67,7 @@ const PostComponent = ({ publishPost, posts }: any) => {
   return (
     <div className="">
       <div className="flex">
-        <div className="w-full lg:w-2/3 max-w-7xl mx-auto border border-gray-600 h-auto border-t-0">
+        <div className="w-full lg:w-2/3 max-w-7xl mx-auto h-auto ">
           <PostPublishComponent publishPost={publishPost} />
 
           {posts && posts.length > 0 && posts.map((post, index) => <Post key={index} {...post} />)}
